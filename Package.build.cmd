97c6e2a885f58180b2c63ff3bc44d6f5cfb31c64
mattbrailsford <me@mattbrailsford.com>
2014-03-20 17:03:41 +0000

    Removed source from root (now in v6x and v7x branches)

--- Package.build.cmd
+++
@@ -1,4 +0,0 @@
-C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\msbuild.exe Package.Common.build.xml /target:CleanAll
-C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\msbuild.exe Package.DataType.build.xml
-C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\msbuild.exe Package.Web.build.xml
-pause
